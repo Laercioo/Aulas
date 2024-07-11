5c07de5dfae2c04ca8fbec887f0b4da3928b8bbf
Laercio <laerciooliveira293@gmail.com>
2024-07-11 20:54:40 -0300

    Master

---
+++ Aula 10/lousa2.py
@@ -0,0 +1,22 @@
+from selenium.webdriver import firefox
+from selenium.webdriver.common.by import By
+
+browser = firefox()
+
+link = "https://page-test-selenium.s3.sa-east-1.amazonaws.com/index.html"
+
+browser.get(link)
+
+btn_add_element = browser.find_element(By.ID, "addElement")
+btn_add_element.click()
+
+for i in range(20):
+    btn_add_element.click()
+
+# Corrigir para encontrar todos os elementos de input (checkboxes)
+checkboxes = browser.find_elements(By.TAG_NAME, "input")
+
+for checkbox in checkboxes:
+    checkbox.click()
+
+browser.quit()
