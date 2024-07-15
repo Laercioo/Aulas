17f225e88e42a9f84f6b465147437fce13c67e20
Laercio <laerciooliveira293@gmail.com>
2024-07-15 19:17:54 -0300

    Master

--- Automação/Checkboxautomação.py
+++ Automação/Checkboxautomação.py
@@ -1,22 +1,50 @@
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import time
 
 browser = Firefox()
 
-link = "https://page-test-selenium.s3.sa-east-1.amazonaws.com/index.html"
-
+link = 'https://google.com'
 browser.get(link)
 
-btn_add_element = browser.find_element(By.ID, "addElement")
-btn_add_element.click()
+input_area = browser.find_element(By.NAME, "q")
+input_area.send_keys('Instituto Joga Junto')
+input_area.send_keys(Keys.ENTER)
 
-for i in range(20):
-    btn_add_element.click()
+time.sleep(10) 
 
-# Corrigir para encontrar todos os elementos de input (checkboxes)
-checkboxes = browser.find_elements(By.TAG_NAME, "input")
+result_search = browser.find_elements(By.TAG_NAME, 'h3')
 
-for checkbox in checkboxes:
-    checkbox.click()
+check = False
+
+for result in result_search:
+    if 'Instituto Joga Junto' in result.text:
+        result.click()
+        print("Link encontrado e clicado!")
+        check = True
+        break
+
+title = browser.title
+assert 'Instituto Joga Junto' in title, "Página incorreta na busca"
+
+input_nome = browser.find_element(By.ID, "nome")
+input_nome.send_keys('Laercio Alexandre de Oliveira')
+
+input_email = browser.find_element(By.ID, "email")
+input_email.send_keys('laerciooliveira293@gmail.com')
+
+input_assunto = browser.find_element(By.ID, "assunto")
+select = Select(input_assunto)
+select.select_by_index(5)
+
+input_msg = browser.find_element(By.ID, "mensagem")
+input_msg.send_keys('Meu primeiro script de automação - Squad 6')
+
+button_enviar = browser.find_element(By.XPATH, "//button[@type='submit']")
+button_enviar.click()
+
+time.sleep(10)
 
 browser.quit()
