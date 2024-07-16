fd8b6b8626f2ba60c3f4d0bc22817232379ab276
Laercio <laerciooliveira293@gmail.com>
2024-07-15 21:12:19 -0300

    Master

---
+++ features/contatoijj.feature
@@ -0,0 +1,11 @@
+#language: pt
+
+
+Funcionalidade: Envio de dados ao formulario
+    Cenário: Envio de dados com assunto quero ser facilitador
+
+    Dado que estou na página do instituto joga junto
+    Quando prencho p formulario de contato
+    E aperto o botão de enviar
+    Então os dados são recebidos com sucesso
+
---
+++ features/steps/contatobehave.py
@@ -0,0 +1,31 @@
+from behave import given, when, then
+from selenium.webdriver import Firefox()
+from selenium.webdriver,common.by import By 
+from selenium.webdriver.support.ui import Select
+
+
+@given(u'que estou na página do instituto joga junto')
+def step_on_instituto_page(context):
+    context.browser = Firefox()
+    context.browser.get('https://www.jogajuntoinstituto.org/')
+    
+    browser_title = context.browser.title
+    assert 'Instituto Joga Junto' in browser_title, "Titulo não encontrado"
+    
+    
+@when(u'prencho p formulario de contato')
+def step_impl(context):
+    context.browser.find_elements_by(By.name, 'e-mail').send_keys('email@gmail.com')
+    context.browser.find_elements_by(By.name, 'nome').send_keys('João da Silva')
+    context.browser.find_elements_by(By.name, 'body').send_keys('Minha primeira automação com behave')
+    select_subjects = context.browser.find_element_by(By.name, 'assunto'). send_keys('Minha primeira automação com behave')
+    select = Select(select_subjets)
+
+
+@when(u'aperto o botão de enviar')
+def step_impl(context):
+    pass
+
+@then(u'os dados são recebidos com sucesso')
+def step_impl(context):
+    pass
